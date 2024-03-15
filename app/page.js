e41f73247b0620fb09312cbbc7d6f710fedf78b3
felipesandoli <cfelipesandoli@gmail.com>
2024-03-15 22:08:49 +0000

    Add background and cards layout

--- app/page.js
+++ app/page.js
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main>
-      
+      <div className="background"></div>
+      <div className="card-container">
+        <div className="card-front"></div>
+        <div className="card-back"></div>
+      </div>
     </main>
   );
 }
